d9efa466fb3cd11ea35ede4db6a1abcf5cb3be23
WebDeveloper <wesleydpnt@gmail.com>
2025-11-27 15:27:36 +0100

    Fix port for Railway

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ using FluentValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Railway port configuratie
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 // Add services
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
